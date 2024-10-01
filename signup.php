b611a470a26dd1015497c77fd66d4b68f1f3ce8e
Chrisbells <35466316+Chrisbells@users.noreply.github.com>
2024-10-01 16:24:55 -0400

    fix password hash algorithm, respond 400 error when non matching passwords

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = strtolower($_POST['email']);
-    $password = hash('sha256', $_POST['password']); //unsure if sha256 is good algo for hashing passwords
+    $password = password_hash('sha256', $_POST['password']); 
     $nickname = $_POST['nickname'];
     $country = $_POST['country'];
     $timezone = $_POST['timezone'];
@@ -24,8 +24,12 @@
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
+    if($_POST["confirmPassword"]!=$_POST["password"]){
     $result = mysqli->query("SELECT MAX(USER_ID) FROM cse442_2024_fall_team_ak_db.ACCOUNTS");
     $user_id = $result ++;
-    $mysqli->query("INSERT INTO cse442_2024_fall_team_ak_db.ACCOUNTS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, NICKNAME, COUNTRY, TIMEZONE, GENDER, LANG VALUES('$user_id', '$first_name', '$last_name', '$email', '$password', '$nickname', '$country', '$timezone', '$gender', '$language')");
+    $mysqli->query("INSERT INTO cse442_2024_fall_team_ak_db.ACCOUNTS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, NICKNAME, COUNTRY, TIMEZONE, GENDER, LANG VALUES('$user_id', '$first_name', '$last_name', '$email', '$password', '$nickname', '$country', '$timezone', '$gender', '$language')");    
+    } else {
+        http_response_code(400);
+    }
 
 ?>

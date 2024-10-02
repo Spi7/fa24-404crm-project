f30ceec8a9fa72baae91b88f10c3328437ffac76
Jared Lamanna <jmlamann@buffalo.edu>
2024-10-02 17:23:12 -0400

    fixed variables and POST names, added database credentials

--- reset_password.php
+++ reset_password.php
@@ -1,21 +1,21 @@
 <?php
 
     $email = $_POST['email'];
-    $new_password = $_POST['new_password'];
-    $confirm_password = $_POST['confirm_password'];
+    $newPassword = $_POST['newPassword'];
+    $confirmPassword = $_POST['confirmPassword'];
 
     //user and password need to be someone's login credentials for the server?
     //safe to have that in our code?
-    $mysqli = new mysqli("localhost", "", "", "cse442_2024_fall_team_ak_db");
+    $mysqli = new mysqli("cattle", "jmlamann", "50307671", "cse442_2024_fall_team_ak_db");
     // Check connection
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
 
     //confirm passwords match
-    if(strcmp($new_password, $confirm_password) == 0){
-        $new_password = password_hash($new_password, PASSWORD_DEFAULT);
-        $mysqli->query("UPDATE cse442_2024_fall_team_ak_db.ACCOUNTS SET PASSWORD='$new_password' WHERE EMAIL='$email'");
+    if(strcmp($newPassword, $confirmPassword) == 0){
+        $newPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+        $mysqli->query("UPDATE cse442_2024_fall_team_ak_db.ACCOUNTS SET PASSWORD='$newPassword' WHERE EMAIL='$email'");
     }
     else{
         http_response_code(400);

3048e03d92e91b892b2cba28cd6609c68c46e94e
Nick <ngaow29@gmail.com>
2024-10-02 23:52:47 -0400

    Changed the word account on line 11 & 20 the uppercase (same to database)

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
         die("Connection failed: " . $mysqli->connect_error);
     }
     if(isset($_POST['email'])) {
-        $query = "SELECT password FROM cse442_2024_fall_team_ak_db.Accounts WHERE email='$email'";
+        $query = "SELECT password FROM cse442_2024_fall_team_ak_db.ACCOUNTS WHERE email='$email'";
         $result = $mysqli->query(query: $query);
         if($result->num_rows == 1){
             $row=$result->fetch_assoc();
@@ -17,7 +17,7 @@
                 $session_token = bin2hex(random_bytes(32));
                 setcookie("SESSION_TOKEN",$session_token,  time()+86400,"/");//86400    seconds in a day
                 header('Location: home.php');
-                $updateSessionTokenQuery="UPDATE cse442_2024_fall_team_ak_db.accounts SET SESSION_TOKEN='$session_token' WHERE email='$email'";
+                $updateSessionTokenQuery="UPDATE cse442_2024_fall_team_ak_db.ACCOUNTS SET SESSION_TOKEN='$session_token' WHERE email='$email'";
                 $result = $mysqli->query($updateSessionTokenQuery);
             } else{
                 echo "login unsucessful";

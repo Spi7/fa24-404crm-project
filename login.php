1f0f89ab594a4f2ec30307a78e66f0980011ed99
Nick <ngaow29@gmail.com>
2024-10-02 23:33:45 -0400

    css relocate, login html & php remodified

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
     $email = strtolower($_POST['email']);
     $password = $_POST['password'];
 
-    $mysqli = new mysqli("localhost", "root", "", "users");
+    $mysqli = new mysqli("cattle", "jmlamann", "50307671", "cse442_2024_fall_team_ak_db");
     // Check connection
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
     if(isset($_POST['email'])) {
-        $query = "SELECT password FROM users.Accounts WHERE email='$email'";
+        $query = "SELECT password FROM cse442_2024_fall_team_ak_db.Accounts WHERE email='$email'";
         $result = $mysqli->query(query: $query);
         if($result->num_rows == 1){
             $row=$result->fetch_assoc();
@@ -17,7 +17,7 @@
                 $session_token = bin2hex(random_bytes(32));
                 setcookie("SESSION_TOKEN",$session_token,  time()+86400,"/");//86400    seconds in a day
                 header('Location: home.php');
-                $updateSessionTokenQuery="UPDATE users.accounts SET SESSION_TOKEN='$session_token' WHERE email='$email'";
+                $updateSessionTokenQuery="UPDATE cse442_2024_fall_team_ak_db.accounts SET SESSION_TOKEN='$session_token' WHERE email='$email'";
                 $result = $mysqli->query($updateSessionTokenQuery);
             } else{
                 echo "login unsucessful";

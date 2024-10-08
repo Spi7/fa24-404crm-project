da6e1d558c12a18bf0eed06714474ecd9c13f3c3
Jared Lamanna <jmlamann@buffalo.edu>
2024-10-08 17:39:53 -0400

    added month/year selection

--- calendar-feature/calendar.php
+++ calendar-feature/calendar.php
@@ -21,7 +21,7 @@
             <a href="addevent.html">
                 <img src="../img/add icon.png" alt="Add Note">
             </a>
-        </div> 
+        </div>
     </header>
     <div class="calendar">
         <div class="days-row">
@@ -69,6 +69,26 @@
             <div class="day-box">31</div>
             <div class="day-box">1</div>
             <div class="day-box">2</div>
+        </div>
+        <div class="footer-container-calendar">
+        <label for="month">Change Month/Year: </label>
+                    <!-- Dropdowns to select a specific month and year -->
+                    <select id="month" onchange="setDate()">
+                        <option value=0>Jan</option>
+                        <option value=1>Feb</option>
+                        <option value=2>Mar</option>
+                        <option value=3>Apr</option>
+                        <option value=4>May</option>
+                        <option value=5>Jun</option>
+                        <option value=6>Jul</option>
+                        <option value=7>Aug</option>
+                        <option value=8>Sep</option>
+                        <option value=9>Oct</option>
+                        <option value=10>Nov</option>
+                        <option value=11>Dec</option>
+                    </select>
+                    <!-- Dropdown to select a specific year -->
+                    <select id="year" onchange="setDate()"></select>
         </div>
     </div>
 </div>

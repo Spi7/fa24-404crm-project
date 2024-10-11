ebf2efe8b4888061584f95d6586a71a56e3ee3d6
andempsey8 <143447679+andempsey8@users.noreply.github.com>
2024-10-11 12:37:26 -0400

    Update calendar.php
    Set timezone and added day-abbr divs for mobile compatibility

--- calendar-feature/calendar.php
+++ calendar-feature/calendar.php
@@ -17,7 +17,8 @@
         <button id="prev-month">Prev</button>
         <div class="current-date">
             <h1 class="month" id="month">October 2024</h1>
-            <?php $currentDate = date("F j, Y"); 
+            <?php date_default_timezone_set('America/New_York');
+            $currentDate = date("F j, Y"); 
             echo "<h1>Today's Date: $currentDate</h1>";?>
         </div>
         <button id="next-month">Next</button>
@@ -35,12 +36,19 @@
     <div class="calendar">
         <div class="days-row">
             <div class="day">Sunday</div>
+            <div class="day-abbr">Su</div>
             <div class="day">Monday</div>
+            <div class="day-abbr">M</div>
             <div class="day">Tuesday</div>
+            <div class="day-abbr">Tu</div>
             <div class="day">Wednesday</div>
+            <div class="day-abbr">W</div>
             <div class="day">Thursday</div>
+            <div class="day-abbr">Th</div>
             <div class="day">Friday</div>
+            <div class="day-abbr">F</div>
             <div class="day">Saturday</div>
+            <div class="day-abbr">Sa</div>
         </div>
         <div class="calendar-grid" id="calendar-grid">
             <div class="day-box">29</div>

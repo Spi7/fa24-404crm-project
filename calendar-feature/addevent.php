c32293200b00ef6fa8950795ebe9ee906e2e2b39
Jared Lamanna <jmlamann@buffalo.edu>
2024-10-10 20:23:25 -0400

    implemented functionality for creating a new event in the CALENDARS table, have not tested yet

---
+++ calendar-feature/addevent.php
@@ -0,0 +1,23 @@
+<?php
+    include('../db_connection.php');
+    connectDB();
+    if(isset($_COOKIE['SESSION_TOKEN'])) {
+        $query = "SELECT USER_ID FROM ACCOUNTS WHERE session_token='$_COOKIE[SESSION_TOKEN]'";
+        $result = $mysqli->query(query: $query);
+        if($result->num_rows == 1){
+            $row = $result->fetch_assoc();
+            $user_id = $row["USER_ID"];
+            $result = $mysqli->query("SELECT MAX(EVENT_ID) as max FROM ACCOUNTS");
+            $event_id = $result->fetch_assoc()["max"]+1;
+            $event_date = $_POST["EVENT_DATE"];
+            $frequency = $_POST["FREQUENCY"]
+            $title = $_POST["TITLE"]:
+            $description = $_POST["DESCRIPTION"];
+            $mysqli->query("INSERT INTO CALENDARS(EVENT_ID, USER_ID, EVENT_DATE, FREQUENCY, TITLE, DESCRIPTION) VALUES('$event_id', '$user_id', '$event_date', '$frequency', '$title', '$description')");
+        } else {
+            echo "session token not found";
+        }
+    }else{
+        echo "no session token";
+    }
+?>

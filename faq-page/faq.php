68cff24f0d44cb8a2617d1e1f95a708d69403c47
Chrisbells <35466316+Chrisbells@users.noreply.github.com>
2024-10-25 12:02:25 -0400

    created page for faq

---
+++ faq-page/faq.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <title>404 Not Found Application FAQ</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='faq.css'>
+</head>
+
+<body>
+    <header>
+        <h2>404 Not Found Application FAQ</h2>
+        <img src="../img/404 not found.png" alt="404 Not Found Logo">
+    </header>
+    <div id="faqContent">
+        <h3>Login and account FAQ</h3>
+        <ul>
+            <li class="question">
+                Why can't I find my password reset token email?
+            </li>
+            <li class="answer">
+                Try checking your email spam and unwanted folder and
+                make sure you are using the correct email. The email subject should read: 404 crm reset token and come
+                from www-data@buffalo.edu
+            </li>
+            <li class="question">
+                Why is my password reset token invalid?
+            </li>
+            <li class="answer">
+                Try checking your input for any extra or missing characters
+                copied and pasted from the email as one change will cause it to not work, also make sure its been less
+                than 15 minutes as they expire
+            </li>
+            <li class="question">
+                Are emails case sensitive?
+            </li>
+            <li class="answer">
+                No when typing your email there is no need to stress about capitalization.
+            </li>
+            <li class="question">
+                How do I edit my username?
+            </li>
+            <li class="answer">
+                When logged in click on your user profile picture in the upper left corner,
+                this will bring you to the user info page, when here you can click on change username and it will bring
+                you to the change username page
+            </li>
+        </ul>
+        <h3>
+            Calendar FAQ
+        </h3>
+        <ul>
+            <li class="question">
+                How do I add an event to my calendar?
+            </li>
+            <li class="answer">When on the calendar page in the top right hand corner there is a
+                plus button, clicking this will bring you to the create event page where you can add to your calendar
+            </li>
+        </ul>
+        <h3>
+            Chat FAQ
+        </h3>
+        <ul>
+            <li class="question">
+                How come when I add a contact the app says "Please enter a valid email address."?
+            </li>
+            <li class="answer"> Currently we only
+                support messaging users who are on the platform enter a valid email address means their email was
+                misspelled or they are not on the platform
+            </li>
+        </ul>
+        <h3>
+            Invoice FAQ
+        </h3>
+        <ul>
+            <li class="question">
+                How come when I try to invoice someone it says user not found
+            </li>
+            <li class="answer">Currently we only support invoicing users who are on the platform enter a valid email
+                address means
+                their email was misspelled or they are not on the platform
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>

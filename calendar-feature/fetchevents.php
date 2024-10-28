94595a958c8e2fc62ad8f3e8a5832752ba379e72
Jared Lamanna <jmlamann@buffalo.edu>
2024-10-28 17:46:25 -0400

    fixed a bug where signed in user received error "session token not found" while signed in

--- calendar-feature/fetchevents.php
+++ calendar-feature/fetchevents.php
@@ -3,7 +3,7 @@
     connectDB();
     if(isset($_COOKIE['SESSION_TOKEN'])) {
         $query = "SELECT USER_ID FROM ACCOUNTS WHERE session_token='$_COOKIE[SESSION_TOKEN]'";
-        $mysqli->query($query);
+        $result = $mysqli->query($query);
         if($result->num_rows == 1) {
             $row = $result->fetch_assoc();
             $user_id = $row["USER_ID"];

2cd7836540dbc17bd3b67077df18fa1127de8a46
Chrisbells <35466316+Chrisbells@users.noreply.github.com>
2024-11-03 19:47:34 -0500

    fix invoices page breaking for invoiceless account, also add address header to table as it was missing

--- invoice-feature/invoices.php
+++ invoice-feature/invoices.php
@@ -3,37 +3,42 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 set_error_handler("var_dump");
 
-    include "../db_connection.php";
-    connectDB();
-    $userIDQuery = $mysqli->query("SELECT USER_ID FROM ACCOUNTS WHERE SESSION_TOKEN='$_COOKIE[SESSION_TOKEN]'");
-    if ($userIDQuery->num_rows == 0) {
-        die ("error session token not found");
-    }
-    $userID = $userIDQuery->fetch_assoc()["USER_ID"];
-    //get dest id - already checked there is a row before
-    $invoices= $mysqli->query("SELECT INVOICE_ID FROM INVOICES WHERE SENDER='$userID' or RECIPIENT='$userID'");
-    $invoiceIDs=[];
-    while( $invoiceID = $invoices->fetch_column(0) ) {
-        array_push($invoiceIDs,$invoiceID);
-    }
-    $invoicesInfo= $mysqli->query("SELECT * FROM INVOICES WHERE SENDER='$userID' or RECIPIENT='$userID'");
-    $invoiceItems = $mysqli->query("SELECT * FROM INVOICE_ITEMS WHERE INVOICE_ID IN (" . implode(',',$invoiceIDs) . ")");
-    
+include "../db_connection.php";
+connectDB();
+$userIDQuery = $mysqli->query("SELECT USER_ID FROM ACCOUNTS WHERE SESSION_TOKEN='$_COOKIE[SESSION_TOKEN]'");
+if ($userIDQuery->num_rows == 0) {
+    die("error session token not found");
+}
+$userID = $userIDQuery->fetch_assoc()["USER_ID"];
+//get dest id - already checked there is a row before
+$invoices = $mysqli->query("SELECT INVOICE_ID FROM INVOICES WHERE SENDER='$userID' or RECIPIENT='$userID'");
+$invoiceIDs = [];
+while ($invoiceID = $invoices->fetch_column(0)) {
+    array_push($invoiceIDs, $invoiceID);
+}
+$noInvoices = true;
+$invoicesInfo = $mysqli->query("SELECT * FROM INVOICES WHERE SENDER='$userID' or RECIPIENT='$userID'");
+if ($invoicesInfo->num_rows != 0) {
+    $noInvoices = false;
+    $invoiceItems = $mysqli->query("SELECT * FROM INVOICE_ITEMS WHERE INVOICE_ID IN (" . implode(',', $invoiceIDs) . ")");
+}
 
-    // $mysqli->query(query: "INSERT INTO INVOICES VALUES ('$invoiceID','$senderID','$destUserId',now(),'$_POST[invoiceDueDate]','$_POST[invoiceBA]','$_POST[invoiceName]')");
-    // for ($i = 0; $i < count(value: $quantities); $i++) {
-    //     $invoiceItemID = (int) $mysqli->query("SELECT MAX(ITEM_ID) as max FROM INVOICE_ITEMS")->fetch_assoc()["max"] + 1;
-    //     $mysqli->query("INSERT INTO INVOICE_ITEMS VALUES ('$invoiceItemID','$invoiceID','$quantities[$i]','$prices[$i]','$descriptions[$i]')");
-    // }
+// $mysqli->query(query: "INSERT INTO INVOICES VALUES ('$invoiceID','$senderID','$destUserId',now(),'$_POST[invoiceDueDate]','$_POST[invoiceBA]','$_POST[invoiceName]')");
+// for ($i = 0; $i < count(value: $quantities); $i++) {
+//     $invoiceItemID = (int) $mysqli->query("SELECT MAX(ITEM_ID) as max FROM INVOICE_ITEMS")->fetch_assoc()["max"] + 1;
+//     $mysqli->query("INSERT INTO INVOICE_ITEMS VALUES ('$invoiceItemID','$invoiceID','$quantities[$i]','$prices[$i]','$descriptions[$i]')");
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Invoices</title>
     <link rel="stylesheet" href="invoices.css"> <!-- Link to external CSS file -->
 </head>
+
 <body>
     <a href="../home-page/home.php" class="button" id="homeButtonWrapper">
         <img id="homeButton" src="../img/home.png" alt="Home">
@@ -47,15 +52,18 @@ set_error_handler("var_dump");
                 <th>Recipient</th>
                 <th>Created</th>
                 <th>Due</th>
+                <th>Address</th>
                 <th>Description</th>
             </tr>
             <?php
-            while($row=$invoicesInfo->fetch_row()){
-                echo "<tr>";
-                foreach ($row as $value) {
-                    echo "<td>".$value."</td>";
+            if ($noInvoices == false) {
+                while ($row = $invoicesInfo->fetch_row()) {
+                    echo "<tr>";
+                    foreach ($row as $value) {
+                        echo "<td>" . $value . "</td>";
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
             }
             ?>
         </table>
@@ -69,15 +77,18 @@ set_error_handler("var_dump");
                 <th>Description</th>
             </tr>
             <?php
-            while($row=$invoiceItems->fetch_row()){
-                echo "<tr>";
-                foreach ($row as $value) {
-                    echo "<td>".$value."</td>";
+            if ($noInvoices == false) {
+                while ($row = $invoiceItems->fetch_row()) {
+                    echo "<tr>";
+                    foreach ($row as $value) {
+                        echo "<td>" . $value . "</td>";
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
             }
             ?>
         </table>
     </div>
 </body>
+
 </html>

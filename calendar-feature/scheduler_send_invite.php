5954217ebe881f7128bee4fce3c8b1cc14870c5a
Jared Lamanna <jmlamann@buffalo.edu>
2024-11-14 15:13:43 -0500

    now accepts POST for event_id and also inserts event_id to the SCHEDULER_INVITES table, still untested

--- calendar-feature/scheduler_send_invite.php
+++ calendar-feature/scheduler_send_invite.php
@@ -6,12 +6,13 @@
 
     $user_id = $user['USER_ID'];
     $recipient_id = $_POST['RECIPIENT_ID'];
+    $event_id = $_POST['EVENT_ID']
     //get max id , +1 for next valid id
     $query = "SELECT MAX(INVITE_ID) as max FROM SCHEDULER_INVITES";
     $result = $mysqli->query($query);
     $invite_id = $result->fetch_assoc()["max"]+1;
     //insert the invite into the table
-    $query = "INSERT INTO SCHEDULER_INVITES (INVITE_ID, SENDER_ID, RECIPIENT_ID) VALUES ('$invite_id', '$user_id', '$recipient_id')";
+    $query = "INSERT INTO SCHEDULER_INVITES (INVITE_ID, EVENT_ID, SENDER_ID, RECIPIENT_ID) VALUES ('$invite_id', '$event_id', '$user_id', '$recipient_id')";
     $mysqli->query($query);
 
 ?>

3a17f5e6c37cf176a674546ef9356d88bd6a24ef
Jared Lamanna <jmlamann@buffalo.edu>
2024-11-20 17:52:34 -0500

    now uses prepared statement

--- notifications.php
+++ notifications.php
@@ -2,8 +2,10 @@
 
     function createNotification($sender_id, $recipient_id, $table, $link_id){
         global $mysqli;
-        $query = "INSERT INTO NOTIFICATIONS (SENDER_ID, RECIPIENT_ID, NOTIFICATION_TYPE, LINK_ID) VALUES ('$sender_id', '$recipient_id', '$table', '$link_id')";
-        $mysqli->query($query);
+        $query = "INSERT INTO NOTIFICATIONS (SENDER_ID, RECIPIENT_ID, NOTIFICATION_TYPE, LINK_ID) VALUES (?, ?, ?, ?)";
+        $stmt = $mysqli->prepare($query);
+        $stmt->bind_param("iisi", $sender_id, $recipient_id, $table, $link_id);
+        $stmt->execute();
     }
 
 ?>

1eb077faf4f37e5a9934821ad7110e523e0296ab
Jared Lamanna <jmlamann@buffalo.edu>
2024-11-20 18:04:03 -0500

    SCHEDULER_INVITES table is now redundant due to NOTIFICATIONS table, directly creates calendar event invites as notifications

--- calendar-feature/scheduler_send_invite.php
+++ calendar-feature/scheduler_send_invite.php
@@ -8,16 +8,9 @@
     $user_id = $user['USER_ID'];
     $recipient_id = $_POST['select-contact'];
     $event_id = $_POST['select-event'];
-    //get max id , +1 for next valid id
-    $query = "SELECT MAX(INVITE_ID) as max FROM SCHEDULER_INVITES";
-    $result = $mysqli->query($query);
-    $invite_id = $result->fetch_assoc()["max"]+1;
-    //insert the invite into the table
-    $query = "INSERT INTO SCHEDULER_INVITES (INVITE_ID, EVENT_ID, SENDER_ID, RECIPIENT_ID) VALUES ('$invite_id', '$event_id', '$user_id', '$recipient_id')";
-    $mysqli->query($query);
 
     // this makes the SCHEDULER_INVITES table redundant? instead of linking to SCHEDULER_INVITES, link directly to the CALENDARS event?
-    createNotification($user_id, $recipient_id, "SCHEDULER_INVITES", $invite_id);
+    createNotification($user_id, $recipient_id, "CALENDARS", $event_id);
 
     //redirect to calendar and exit
     header('Location: calendar.php');

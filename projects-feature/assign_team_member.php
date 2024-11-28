55fe7087d53d8c125b96c0ab3bd72747167af352
JijieYuan <113218557+JijieYuan@users.noreply.github.com>
2024-11-28 11:26:18 -0500

    Delete the debug part

--- projects-feature/assign_team_member.php
+++ projects-feature/assign_team_member.php
@@ -1,8 +1,4 @@
 <?php
-// Enable error reporting for debugging
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include('../db_connection.php');
 include('../notifications.php'); // Include notifications logic
 connectDB();

75cd91d5e672ddc877e37c6c71b55dbfa86c9d85
Jared Lamanna <jmlamann@buffalo.edu>
2024-12-01 12:37:45 -0500

    removed redundnat query to select max EVENT_ID, this column is set to auto increment in the database

--- calendar-feature/addevent.php
+++ calendar-feature/addevent.php
@@ -4,8 +4,6 @@
     fetchUserData();
 
     $user_id = $user['USER_ID'];
-    $result = $mysqli->query("SELECT MAX(EVENT_ID) as max FROM CALENDARS");
-    $event_id = $result->fetch_assoc()["max"]+1;
     $event_start = $_POST["start-time"];
     $event_end = $_POST["end-time"];
     $frequency = $_POST["repeat"];
@@ -14,7 +12,7 @@
     $title = $_POST["event-title"];
     $description = $_POST["event-description"];
     $color = $_POST["color"];
-    $mysqli->query("INSERT INTO CALENDARS (EVENT_ID, USER_ID, EVENT_START, FREQUENCY, TITLE, EVENT_DESCRIPTION, EVENT_END, COLOR, ALL_DAY) VALUES ('$event_id', '$user_id', '$event_start', '$frequency', '$title', '$description', '$event_end', '$color', '$all_day')");
+    $mysqli->query("INSERT INTO CALENDARS (USER_ID, EVENT_START, FREQUENCY, TITLE, EVENT_DESCRIPTION, EVENT_END, COLOR, ALL_DAY) VALUES ('$user_id', '$event_start', '$frequency', '$title', '$description', '$event_end', '$color', '$all_day')");
     header('Location: calendar.php');
     exit();
 ?>
